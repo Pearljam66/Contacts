7c52c5dc01abc0256f2304fb8affe19eda0617c2
Sarah <sarahireneclark@gmail.com>
2025-03-04 15:50:03 -0600

    Initial commit of project

---
+++ ContactsFun/ContactsFunTests/ContactsFunTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsFunTests.swift
+//  ContactsFunTests
+//
+//  Created by Sarah Clark on 3/4/25.
+//
+
+import Testing
+@testable import ContactsFun
+
+struct ContactsFunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
